400c82ed628e15fb2bd05bd6af66037707bb69d0
MistbornTC <pokeydokeyokey@gmail.com>
2025-09-15 18:51:40 +0100

    Update torn-gym-ratios.meta.js

--- torn-gym-ratios.meta.js
+++ torn-gym-ratios.meta.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         Torn Gym Ratios
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.21
 // @description  Gym training helper with target percentages and current distribution display
 // @author       Mistborn [3037268]
 // @match        https://www.torn.com/gym.php*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
 // @grant        GM_setValue
 // @grant        GM_getValue
-// @run-at       document-end
+// @run-at       document-body
+// @license      MIT
 // @downloadURL  https://github.com/MistbornTC/torn-gym-ratios/raw/main/torn-gym-ratios.user.js
 // @updateURL    https://github.com/MistbornTC/torn-gym-ratios/raw/main/torn-gym-ratios.meta.js
 // @supportURL   https://github.com/MistbornTC/torn-gym-ratios/issues
